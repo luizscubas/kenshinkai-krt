8cf183b115a2d412dd5e4e68ba0d4f98f5dc678a
Luiz - DevBuilder <lhs.cubas@gmail.com>
2026-09-09 01:14:33 -0300

    chore: update site URL and configure base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://kenshinkai.com.br',
+  site: 'https://luizscubas.github.io',
+  base: '/kenshinkai-krt',
   integrations: [
     tailwind({
       applyBaseStyles: false,
